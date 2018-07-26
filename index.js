fb1b40050d304b8dcb72699db96fc6baf43937ca
Matt Farmer <mrfarmer777@gmail.com>
2018-07-26 04:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
-let currentUser="Grace Hopper"
-let welcomeMessage=`Welcome to Flatbook, ${currentUser}!`
-let excitedWelcomeMessage=welcomeMessage.toUpperCase()
+let currentUser="Grace Hopper";
+let welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage=welcomeMessage.toUpperCase();
